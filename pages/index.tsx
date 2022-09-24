72c98fec71fd370205e176df7f1da2c5b78d3ebd
Carlos Velasco <carlos.velasco@symph.co>
2022-09-24 14:39:07 +0800

    feat: add layouts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import PrimaryLayout from '../components/layouts/PrimaryLayout/PrimaryLayout';
+import SidebarLayout from '../components/layouts/SidebarLayout/SidebarLayout';
 import styles from '../styles/Home.module.css';
+import { NextPageWithLayout } from './page';
 
-const Home: NextPage = () => {
+const Home: NextPageWithLayout = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -70,3 +72,12 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+Home.getLayout = (page) => {
+  return (
+    <PrimaryLayout>
+      <SidebarLayout />
+      {page}
+    </PrimaryLayout>
+  );
+};
